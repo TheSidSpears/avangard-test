670f5e7d9a86a9727207907740756dccce5ef46d
Artur Grigorev <thesidspears@gmail.com>
2020-09-28 17:32:31 +0300

    Hide api key

--- config/weather_api.php
+++ config/weather_api.php
@@ -5,6 +5,6 @@ return [
         'bryansk' => [53.2521, 34.3717]
     ],
     'yandex' => [
-        'api_key' => 'd08d6d52-8b3a-4c14-8d55-84ac6b9ebaac'
+        'api_key' => env('YANDEX_API_KEY')
     ]
 ];
